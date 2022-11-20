5fd26c813dda0c94b0e2949ee3dbb67a3bf40323
Lucas Ernst <lernst1@hs-mittweida.de>
2022-11-20 15:47:23 +0100

    Fixed DateTime Reading

--- CBHPredictorWebAPI/CBHPredictorWebAPI/Controllers/ExcelReadController.cs
+++ CBHPredictorWebAPI/CBHPredictorWebAPI/Controllers/ExcelReadController.cs
@@ -24,20 +24,20 @@ namespace CBHPredictorWebAPI.Controllers
                     var rowCount = worksheet.Dimension.Rows;
                     for(int row = 2; row < rowCount; row++)
                     {
-                        list.Add(new LeadEntry() { 
+                        list.Add(new LeadEntry() {
                             leadID= Convert.ToInt32(worksheet.Cells[row,1].Value),
                             leadNo= worksheet.Cells[row,2].Value.ToString(),
                             leadStatus= worksheet.Cells[row,3].Value.ToString(),
-                            leadDate = (DateTime)worksheet.Cells[row,4].Value,
-                            organisationID = Convert.ToInt32(worksheet.Cells[row,5].Value),
-                            countryID = Convert.ToInt32(worksheet.Cells[row,6].Value),
-                            channel= Convert.ToInt32(worksheet.Cells[row,7].Value),
-                            fieldOfInterest= worksheet.Cells[row,8].Value.ToString(),
-                            specificOfInterest= worksheet.Cells[row,9].Value.ToString(),
-                            paramOfInterest= worksheet.Cells[row,10].Value.ToString(),
-                            diagnosisOfInterest= worksheet.Cells[row,11].Value.ToString(),
-                            matrixOfInterest= worksheet.Cells[row,12].Value.ToString(),
-                            quantityOfInterest = worksheet.Cells[row,13].Value.ToString()
+                            leadDate = DateTime.FromOADate(Convert.ToDouble(worksheet.Cells[row,4].Value))
+                            //organisationID = Convert.ToInt32(worksheet.Cells[row,5].Value),
+                            //countryID = Convert.ToInt32(worksheet.Cells[row,6].Value),
+                            //channel= Convert.ToInt32(worksheet.Cells[row,7].Value),
+                            //fieldOfInterest = worksheet.Cells[row,8].Value.ToString(),
+                            //specificOfInterest = worksheet.Cells[row,9].Value.ToString(),
+                            //paramOfInterest = worksheet.Cells[row,10].Value.ToString(),
+                            //diagnosisOfInterest = worksheet.Cells[row,11].Value.ToString(),
+                            //matrixOfInterest = worksheet.Cells[row,12].Value.ToString(),
+                            //quantityOfInterest = worksheet.Cells[row,13].Value.ToString()
                         });
                     }
                 }    
