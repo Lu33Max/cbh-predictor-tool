f9b683664357138f8b663a072a6e7037501443f3
Ashc0re <62431393+Ashc0re@users.noreply.github.com>
2022-12-22 11:47:36 +0100

    Added Method to create Backups

---
+++ CBHPredictorWebAPI/CBHPredictorWebAPI/Controllers/BackupController.cs
@@ -0,0 +1,65 @@
+﻿using CBHPredictorWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+
+namespace CBHPredictorWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BackupController : Controller
+    {
+        [HttpGet]
+        public async Task<String> CreateBackup()
+        {
+            //Create the object of SqlConnection class to connect with database sql server
+            using (SqlConnection conn = new SqlConnection())
+            {
+                String backupDestination = "G:/backups/";
+
+                if (!Directory.Exists(backupDestination))
+                {
+                    Directory.CreateDirectory(backupDestination);
+                }
+
+                //prepare conectio string
+                conn.ConnectionString = "server =(local); database=CBHDB; Trusted_Connection=True; TrustServerCertificate=True;";
+
+                try
+                {
+                    //Prepare SQL command that we want to query
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandType = CommandType.Text;
+                    // cmd.CommandText = "SELECT * FROM MYTABLE";
+                    cmd.CommandText = "BACKUP DATABASE CBHDB TO DISK = 'G:/backups/CBHDB" + DateTime.Now.ToString("yyyy-MM-dd@HH_mm") + ".bak';";
+                    cmd.Connection = conn;
+
+                    // open database connection.
+                    conn.Open();
+
+                    Console.WriteLine("Connection Open ! ");
+
+                    //Execute the query 
+                    SqlDataReader sdr = cmd.ExecuteReader();
+
+                    ////Retrieve data from table and Display result
+                    while (sdr.Read())
+                    {
+                        int id = (int)sdr["id"];
+                        Console.WriteLine(id);
+                    }
+                    //Close the connection
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Can not open connection !");
+
+                }
+            }
+            return "Success";
+        }
+    }
+}
